908c59af51f9f687b7fa10639a8074b263113c27
randomics <osbitorg@gmail.com>
2016-06-04 16:38:14 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sensorUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sensorUiApp
+ */
+angular.module('sensorUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
